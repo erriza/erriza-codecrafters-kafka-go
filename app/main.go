2f9ef9e24d20f1fb1fe7f2a15367926370796e16
ricsae <eduricsa@hotmail.com>
2025-07-01 12:20:32 -0600

    send parseCorrelationId test1

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -16,7 +17,7 @@ func main() {
 	// message size 32 bit signed integer
 	var message_size int32 = 0
 	// header correlational id
-	var correlational_id int32 = 7
+	// var correlational_id int32 = 7
 
 	fmt.Println("Logs from your program will appear here!")
 	
@@ -25,12 +26,24 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
+	// buf := make([]byte, 1024)
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
+
+		// n, err := conn.Read(buf)
+		// if err == io.EOF {
+		// 	break
+		// }
+		// if err != nil {
+		// 	fmt.Println("error reading", err)
+		// 	return
+		// }
+		
+		correlational_id := ReadCorrelationID(conn, 8)
 		message_size_bytes := Int32ToBytes(message_size, binary.BigEndian)
 		correlational_id_bytes := Int32ToBytes(correlational_id, binary.BigEndian)
 
@@ -44,3 +57,24 @@ func Int32ToBytes(n int32, byteOrder binary.ByteOrder) []byte {
 	byteOrder.PutUint32(buf, uint32(n))
 	return buf
 }
+
+func ReadCorrelationID(conn net.Conn, offset int64) int32 {
+	buff := make([]byte, offset)
+
+	_, err := io.ReadFull(conn, buff)
+	if err != nil {
+		fmt.Println("error reading and skipping offset", err)
+		return 0
+	}
+
+	corrIDbuff := make([]byte, 4)
+	_, err2 := io.ReadFull(conn, corrIDbuff)
+	if err2 != nil {
+		fmt.Println("error reading and skipping offset", err)
+		return 0
+	}
+	
+	correlationalId := binary.BigEndian.Uint32(corrIDbuff)
+	return int32(correlationalId)
+}
+// 00000023 0012 0004 6f7fc661 00096b61666b612d636c69000a6b61666b612d636c6904302e3100
