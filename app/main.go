eb7f49a3cfb342cc063636a2dcb1413353fc7059
ricsae <eduricsa@hotmail.com>
2025-07-08 18:09:21 -0600

    List for unknown topic test1

--- app/main.go
+++ app/main.go
@@ -13,12 +13,6 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// response  message size \ header \ body
-	// message size 32 bit signed integer
-	// var message_size int32 = 0
-	// header correlational id
-	// var correlational_id int32 = 7
-
 	fmt.Println("Logs from your program will appear here!")
 	
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
@@ -50,6 +44,7 @@ func handleConnection(conn net.Conn) {
 }
 
 func handleReq(conn net.Conn) error {
+
 	//Read the header field sequentially. Each call to readBytes
 		message_size, err := ReadBytes(conn, 4)
 		if err != nil { fmt.Println(err) }
@@ -72,7 +67,7 @@ func handleReq(conn net.Conn) error {
 
 		bytesLeftToRead := int(message_size_int) - bytesRead
 
-
+		var request_body []byte
 		if bytesLeftToRead > 0 {
 			_,err := ReadBytes(conn, bytesLeftToRead)
 			if err != nil {
@@ -80,14 +75,28 @@ func handleReq(conn net.Conn) error {
 			}
 		}
 
+		api_key := binary.BigEndian.Uint16(request_api_key)
 		api_version := binary.BigEndian.Uint16(api_version_bytes)
-
 		correlational_id := binary.BigEndian.Uint32(correlational_id_bytes)
 
-		fmt.Println(request_api_key)
+		fmt.Println(api_key)
 		fmt.Println(correlational_id)
 
-	    if api_version > 4 {
+
+		switch api_key {
+
+		case 18:
+			handleApiVersions(conn, api_version, correlational_id_bytes)
+		case 75:
+			handleDescribeTopicPartitions(conn, correlational_id_bytes, request_body)
+		default:
+			fmt.Printf("Unsupported API Key: %d\n", api_key)
+		}
+		return nil
+}
+
+func handleApiVersions(conn net.Conn, api_version uint16, correlational_id_bytes []byte) {
+		    if api_version > 4 {
 			responseSizeBytes := make([]byte, 4)
 			binary.BigEndian.PutUint32(responseSizeBytes, 6) // Size of correlation_id (4) + error_code (2) = 6
 
@@ -103,8 +112,8 @@ func handleReq(conn net.Conn) error {
             binary.BigEndian.PutUint16(errorCodeBytes, 0)
             
             // ApiKeys is a COMPACT_ARRAY. Length is (N+1) as a VARINT.
-            // To declare an array of N=1 element, the length is 2.
-            apiKeysArrayLength := []byte{3} // [2] - means 1 element
+            // To declare an array of N=2 element, the length is 3.
+            apiKeysArrayLength := []byte{3} // [3] - means 2 element
             
             // API Key entry: api_key (18) + min_version (0) + max_version (4)
             apiKeyEntry := make([]byte, 6) // 2 + 2 + 2 = 6 bytes
@@ -147,12 +156,134 @@ func handleReq(conn net.Conn) error {
             binary.BigEndian.PutUint32(responseSizeBytes, uint32(totalResponseSize))
             
             // Write the complete response
+			// response  message size \ header \ body
+			// message size 32 bit signed integer
+			// var message_size int32 = 0
+			// header correlational id
+			// var correlational_id int32 = 7
             conn.Write(responseSizeBytes)
             conn.Write(correlational_id_bytes)
             conn.Write(responseBody)
 		}
-		return nil
 }
+
+func handleDescribeTopicPartitions(conn net.Conn, correlational_id_bytes []byte, request_body []byte) {
+	//Parse the DescribeTopicPartititions request body
+	topicName := parseDescribteTopicPartitionsRequest(request_body)
+
+	// Build the response
+	var responseBody []byte
+
+	// throttle_time_ms (4 bytes) - set to 0
+	throttleTimeBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(throttleTimeBytes, 0)
+	responseBody = append(responseBody, throttleTimeBytes...)
+
+	// topics array - compact array with 1 element, so length is 2
+	topicArrayLength := []byte{2}
+	responseBody = append(responseBody, topicArrayLength...)
+
+	// Topic entry 
+	// error_code (2 bytes) - UNKNOWN_TOPIC_OR_PARTITION = 3
+	errorCodeBytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(errorCodeBytes, 3)
+	responseBody = append(responseBody, errorCodeBytes...)
+
+	// topic_name (compact string)
+	topicNameBytes := encodeCompactString(topicName)
+	responseBody = append(responseBody, topicNameBytes...)
+	
+	// topic_id (16 bytes) - 00000000-0000-0000-0000-000000000000
+	topicIdBytes := make([]byte, 16)
+	responseBody = append(responseBody, topicIdBytes...)
+	
+	// is_internal (1 byte) - false
+	responseBody = append(responseBody, byte(0))
+	
+	// partitions array - empty compact array, so length is 1
+	partitionsArrayLength := []byte{1}
+	responseBody = append(responseBody, partitionsArrayLength...)
+	
+	// topic_authorized_operations (4 bytes) - -2147483648 (not supported)
+	authorizedOpsBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(authorizedOpsBytes, 0x80000000) // -2147483648
+	responseBody = append(responseBody, authorizedOpsBytes...)
+	
+	// tagged fields for topic (empty)
+	responseBody = append(responseBody, byte(0))
+
+	nextCursorBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(nextCursorBytes, 0xFFFFFFFF) // -1
+	responseBody = append(responseBody, nextCursorBytes...)
+
+	// tagged fields for response (empty)
+	responseBody = append(responseBody, byte(0))
+	
+	// Calculate total message size: correlation_id (4) + responseBody
+	totalResponseSize := int32(len(correlational_id_bytes) + len(responseBody))
+	responseSizeBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(responseSizeBytes, uint32(totalResponseSize))
+
+	conn.Write(responseSizeBytes)
+	conn.Write(correlational_id_bytes)
+	conn.Write(responseBody)
+}
+
+func parseDescribteTopicPartitionsRequest(requestBody []byte) string {
+	offset := 0
+
+	// Skip client_id (nullable string)
+	if len(requestBody) > offset {
+		clientIdLength := int(binary.BigEndian.Uint16(requestBody[offset:offset+2]))
+		offset+=2
+		
+		if clientIdLength > 0 {
+			offset += clientIdLength
+		}
+	}
+
+	// skip max_response_bytes (4 bytes)
+	offset += 4
+
+	// skip cursor (nullable)
+	if len(requestBody) > offset {
+		cursorLength := int(binary.BigEndian.Uint32(requestBody[offset:offset+4]))
+		offset+=4
+		if cursorLength > 0 {
+			offset += cursorLength
+		}
+	}
+
+	// Read topics array length (compact array)
+	topicsArrayLength := int(requestBody[offset]) - 1
+	offset += 1
+	
+	if topicsArrayLength > 0 {
+		// Read first topic name (compact string)
+		topicNameLength := int(requestBody[offset]) - 1
+		offset += 1
+		
+		if topicNameLength > 0 {
+			topicName := string(requestBody[offset:offset+topicNameLength])
+			return topicName
+		}
+	}
+	
+	return ""
+}
+
+
+func encodeCompactString(s string) []byte {
+	if s == "" {
+		return []byte{1}
+	}
+
+	result := make([]byte, 1+len(s))
+	result[0] = byte(len(s) + 1) // length + 1
+	copy(result[1:], []byte(s))
+	return  result
+}
+
 
 func ReadBytes(conn net.Conn, bytesToRead int) ([]byte, error) {
 	//read first 8 bytes ignore them
